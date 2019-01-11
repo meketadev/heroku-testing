4e01710d7926bd2a59d4e653a37194a6da2d77a8
tjaegerpca <nicholaskrieg@pensionconsulting.com>
2019-01-11 10:46:16 -0800

    removed bugged copied code

--- herokutesting/routing.py
+++ herokutesting/routing.py
@@ -3,9 +3,6 @@ from django.conf.urls import url
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from chat.consumers import AdminChatConsumer, PublicChatConsumer
-from aprs_news.consumers import APRSNewsConsumer
-
 application = ProtocolTypeRouter({
 
     # WebSocket chat handler
